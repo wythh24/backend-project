9ad5cf3eecebea07c438c0f7f47c362a242788c5
Wythh <88887043+WYTHH@users.noreply.github.com>
2023-02-21 13:53:37 +0700

    create product model

---
+++ Data/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using productstockingv1.models;
+
+namespace productstockingv1.Data;
+
+public class ProductContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public ProductContext(DbContextOptions options) : base(options)
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using productstockingv1.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ProductContext>(
+    options =>
+    {
+        options.UseMySql(builder.Configuration.GetConnectionString("Development"),
+            Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.23-mysql"));
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -13,7 +22,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    // app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
